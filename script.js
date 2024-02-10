7c6145c8fbcdc8096ba36c8344fd4e44230529cc
Hayden Montgomery <hayden.montgomery815@gmail.com>
2024-02-10 14:59:14 -0500

    Finished the search function

--- script.js
+++ script.js
@@ -3,13 +3,26 @@ const suggestions = document.querySelector('.suggestions ul');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
-function search(str) {
+/* function search(str) {
 	let results = [];
 
 	// TODO
+} */
+
+
+/*
+Removed results[] as it is unnesecssary if I return the value from the destructered fruit.filter method.
+This uses the input from the input box and searches the fruit list, regardless of upper or lowercase.
+*/
+function search(str) {
+	const lowerCaseSearch = str.toLowerCase();
+  
+	return fruit.filter(val => {
+	  const lowerCaseFruit = val.toLowerCase();
+	  //console.log(lowerCaseFruit.includes(lowerCaseSearch));
+	  return lowerCaseFruit.includes(lowerCaseSearch);
+	});
 }
-
-
 
 
 function searchHandler(e) {
