ab159a1514f1db985850d6087df8507da1e12ea6
Hayden Montgomery <hayden.montgomery815@gmail.com>
2024-02-10 22:16:21 -0500

    Added mouse over and out event listeners to suggestions. Started work on use suggestions event

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const input = document.querySelector('#fruit');
 const suggestions = document.querySelector('.suggestions ul');
-
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 /* function search(str) {
@@ -37,7 +36,7 @@ function searchHandler(e) {
 function showSuggestions(results, inputVal) {
 	//Cut out all but the first 5 results
 	results = results.slice(0,5);
-	
+
 	//Adds a <li> to the suggestions <div> for each search result
 	results.forEach(val => {
 		const newSuggestion = document.createElement("li");
@@ -48,7 +47,20 @@ function showSuggestions(results, inputVal) {
 
 function useSuggestion(e) {
 	// TODO
+	input.value = e.target.innerText;
+	suggestions.innerHTML = '';
+}
+
+function highlightSuggestion(e) {
+	console.log(e.target.innerText + " Highlighted");
+	console.log(eventDiv);
+}
+
+function removeHighlight(e) {
+	console.log(e.target.innerText + " Unhighlighted");
 }
 
 input.addEventListener('keyup', searchHandler);
 suggestions.addEventListener('click', useSuggestion);
+suggestions.addEventListener('mouseover', highlightSuggestion);
+suggestions.addEventListener('mouseout', removeHighlight);
